5b0611ac696f0d95ea0cdeaffb5c02daecd84dc1
zhanglu <243066208@qq.com>
2020-08-03 10:17:39 +0800

    2020/8/3 更新 => 新增swagger访问映射，解决无法访问swagger页面的问题

--- src/main/java/com/jiebao/platfrom/common/config/UploadFilePathConfig.java
+++ src/main/java/com/jiebao/platfrom/common/config/UploadFilePathConfig.java
@@ -24,6 +24,8 @@ public class UploadFilePathConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler(staticImagePath).addResourceLocations("file:" + imagePath);
         registry.addResourceHandler(staticFilePath).addResourceLocations("file:" + filePath);
+        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
         registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
     }
 }
