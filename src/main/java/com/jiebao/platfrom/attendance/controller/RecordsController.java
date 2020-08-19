1990fc049d2cc62d2789183c1811e8e7fe6f6a85
quantingan <19918830011@163.com>
2020-08-19 17:50:47 +0800

    8月19 号  月度考情导出表

--- src/main/java/com/jiebao/platfrom/attendance/controller/RecordsController.java
+++ src/main/java/com/jiebao/platfrom/attendance/controller/RecordsController.java
@@ -1,15 +1,30 @@
 package com.jiebao.platfrom.attendance.controller;
 
 
+import com.jiebao.platfrom.attendance.daomain.Record;
+import com.jiebao.platfrom.attendance.service.IRecordsService;
+import com.jiebao.platfrom.common.annotation.Log;
 import com.jiebao.platfrom.common.domain.JiebaoResponse;
 import com.jiebao.platfrom.common.domain.QueryRequest;
+import com.jiebao.platfrom.common.utils.CheckExcelUtil;
+import com.jiebao.platfrom.system.dao.UserMapper;
+import com.jiebao.platfrom.system.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Collection;
+import java.util.Locale;
 
 /**
  * <p>
@@ -23,11 +38,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/attendance/record")
 @Api(tags = "attendance-record")
 public class RecordsController {
-
-    @PostMapping("punch")
-    @ApiOperation("打卡接口")
-    public JiebaoResponse punch() {
-        return null;
+    @Autowired
+    IRecordsService recordsService;
+   @Autowired
+   UserService userService;
+    @PostMapping("add")
+    @ApiOperation("月底接口录入")
+    @Log("月底接口录入")
+    public JiebaoResponse add(Record record) {
+        JiebaoResponse jiebaoResponse = new JiebaoResponse();
+        jiebaoResponse = recordsService.saveOrUpdate(record) ? jiebaoResponse.okMessage("月底录入成功") : jiebaoResponse.failMessage("月底录入失败");
+        return jiebaoResponse;
     }
 
 
@@ -35,5 +56,12 @@ public class RecordsController {
     @ApiOperation("打卡记录查询  部门  姓名模糊 时间")
     public JiebaoResponse list(QueryRequest queryRequest, String deptId, String name, String date) {
         return null;
+    }
+
+    @GetMapping("excel")
+    @ApiOperation("导出")
+    public void exportExcel(HttpServletResponse response) {
+
+        CheckExcelUtil.exportExcel(recordsService.list(), response,userService);
     }
 }
