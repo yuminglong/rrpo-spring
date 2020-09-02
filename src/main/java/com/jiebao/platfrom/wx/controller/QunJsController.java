e45c30803978efdcc0ba8d4b0e6f2af2e2267fcc
quantingan <19918830011@163.com>
2020-09-02 17:02:11 +0800

    9月2号 月报评选 附件绑定  查询漏洞修改

--- src/main/java/com/jiebao/platfrom/wx/controller/QunJsController.java
+++ src/main/java/com/jiebao/platfrom/wx/controller/QunJsController.java
@@ -7,11 +7,7 @@ import com.jiebao.platfrom.wx.service.IQunJsService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
@@ -28,7 +24,7 @@ public class QunJsController {
     @Autowired
     IQunJsService qunJsService;
 
-    @GetMapping("saveOrUpdate")
+    @PostMapping("saveOrUpdate")
     @ApiOperation("群 申请单添加修改")
     public JiebaoResponse saveOrUpdate(QunJs qunJs) {
         JiebaoResponse jiebaoResponse = new JiebaoResponse();
@@ -45,7 +41,7 @@ public class QunJsController {
     }
 
 
-    @DeleteMapping("selectById")
+    @GetMapping("selectById")
     @ApiOperation("通过qunid查询对应的值")
     public JiebaoResponse selectById(String qunId) {
         return qunJsService.selectById(qunId);
