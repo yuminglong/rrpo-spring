c700d5ffa08ae129d9e3d959a31fa06e5ef4503a
quantingan <19918830011@163.com>
2020-09-03 17:53:47 +0800

    9月2号 月报评选 附件绑定  查询漏洞修改

---
+++ src/main/java/com/jiebao/platfrom/check/controller/YearBindMenusController.java
@@ -0,0 +1,52 @@
+package com.jiebao.platfrom.check.controller;
+
+
+import com.jiebao.platfrom.check.domain.YearBindMenus;
+import com.jiebao.platfrom.check.service.IYearBindMenusService;
+import com.jiebao.platfrom.common.annotation.Log;
+import com.jiebao.platfrom.common.domain.JiebaoResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 前端控制器
+ * </p>
+ *
+ * @author qta
+ * @since 2020-09-03
+ */
+@RestController
+@RequestMapping("/check/year-bind-menus")
+@Api(tags = "check-考勤年度规则绑定考核类型模块")
+public class YearBindMenusController {
+    @Autowired
+    IYearBindMenusService yearBindMenusService;
+
+    @GetMapping("addOrUpdate")
+    @ApiOperation("考勤年度规则绑定考核类型模块")
+    @Log("绑定 考勤年度规则绑定考核类型模块")
+    public JiebaoResponse addOrUpdate(YearBindMenus yearBindMenus) {
+        return yearBindMenusService.addOrUpdate(yearBindMenus);
+    }
+
+    @DeleteMapping("delete")
+    @ApiOperation("删除")
+    @Log("绑定 删除")
+    public JiebaoResponse delete(String[] ids) {
+        return yearBindMenusService.delete(ids);
+    }
+
+    @GetMapping("list")
+    @ApiOperation("集合")
+    @Log("绑定 考勤年度规则绑定考核类型模块")
+    public JiebaoResponse list(String yearId) {
+        return yearBindMenusService.list(yearId);
+    }
+}
---
+++ src/main/java/com/jiebao/platfrom/check/service/impl/YearBindMenusServiceImpl.java
@@ -0,0 +1,47 @@
+package com.jiebao.platfrom.check.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.jiebao.platfrom.check.domain.YearBindMenus;
+import com.jiebao.platfrom.check.dao.YearBindMenusMapper;
+import com.jiebao.platfrom.check.service.IYearBindMenusService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.jiebao.platfrom.common.domain.JiebaoResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author qta
+ * @since 2020-09-03
+ */
+@Service
+public class YearBindMenusServiceImpl extends ServiceImpl<YearBindMenusMapper, YearBindMenus> implements IYearBindMenusService {
+
+    @Override
+    public JiebaoResponse addOrUpdate(YearBindMenus yearBindMenus) {
+        JiebaoResponse jiebaoResponse = new JiebaoResponse();
+        jiebaoResponse = super.saveOrUpdate(yearBindMenus) ? jiebaoResponse.okMessage("操作成功") : jiebaoResponse.failMessage("操作失败");
+        return jiebaoResponse;
+    }
+
+    @Override
+    public JiebaoResponse delete(String[] ids) {
+        JiebaoResponse jiebaoResponse = new JiebaoResponse();
+        jiebaoResponse = removeByIds(Arrays.asList(ids)) ? jiebaoResponse.okMessage("删除成功") : jiebaoResponse.failMessage("删除失败");
+        return jiebaoResponse;
+    }
+
+    @Override
+    public JiebaoResponse list(String yearId) {
+        QueryWrapper<YearBindMenus> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("year_id", yearId);
+        return new JiebaoResponse().data(list(queryWrapper)).message("查询成功");
+    }
+
+
+}
