950098f6ced8493d2dae591ae23bb505423c2648
quantingan <19918830011@163.com>
2020-09-09 17:00:36 +0800

    9月9号省铁护板当查询 地下所有成功的群

--- src/main/java/com/jiebao/platfrom/check/controller/NumController.java
+++ src/main/java/com/jiebao/platfrom/check/controller/NumController.java
@@ -11,6 +11,7 @@ import io.swagger.annotations.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
@@ -54,7 +55,7 @@ public class NumController {
         return numService.exist(yearId, deptId);
     }
 
-    @GetMapping("deadDate")
+    @PostMapping("deadDate")
     @ApiOperation("逾期回复时间赋值")
     public JiebaoResponse deadDate(String numId, Date date) {
         return numService.deadDate(numId, date);
