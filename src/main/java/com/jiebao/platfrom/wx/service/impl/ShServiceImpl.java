7b74d8c84fcd93ee03565726cf2df73cc5b2bd95
quantingan <19918830011@163.com>
2020-09-11 14:20:56 +0800

    9月10号省铁护板当查询 地下所有成功的群

--- src/main/java/com/jiebao/platfrom/wx/service/impl/ShServiceImpl.java
+++ src/main/java/com/jiebao/platfrom/wx/service/impl/ShServiceImpl.java
@@ -52,7 +52,6 @@ public class ShServiceImpl extends ServiceImpl<ShMapper, Sh> implements IShServi
         sh.setStatus(status);
         sh.setMassage(massage);
         sh.setShDate(new Date());
-        sh.setNumber(qun.getShNumber());
         save(sh);
         qun.setShDate(new Date());
         if (status == 0) {
