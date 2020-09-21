82bde8229222be0e9c8d7ef81fe1fbd8f313baef
quantingan <19918830011@163.com>
2020-09-21 17:35:38 +0800

    9月21 合格群 导出  功能写入

--- src/main/java/com/jiebao/platfrom/wx/service/IQunService.java
+++ src/main/java/com/jiebao/platfrom/wx/service/IQunService.java
@@ -16,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
  * @since 2020-08-20
  */
 public interface IQunService extends IService<Qun> {
-    JiebaoResponse pageList(QueryRequest queryRequest, String name, String userName,Integer status);
+    JiebaoResponse pageList(QueryRequest queryRequest, String name, String userName, Integer status);
 
 //    JiebaoResponse updateStatus(String qunId);
 //
@@ -26,5 +26,10 @@ public interface IQunService extends IService<Qun> {
 
     JiebaoResponse importQun(String content);
 
-    void exPort(HttpServletResponse response, String[] deptId,String workName);//合格群导出
+    void exPort(HttpServletResponse response, String[] deptId, String workName);//合格群导出
+
+    public static void main(String[] args) {
+        int[] nums = {1, 0, -1, 0, -2, 2};
+
+    }
 }
