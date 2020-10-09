b5af031c7e1b9abe9c67c7dbd4bca3b73a21f560
yml <1367422552@qq.com>
2020-10-09 14:24:06 +0800

    2020年8月19日 13:47:26     临时提交

---
+++ src/main/java/com/jiebao/platfrom/common/GuideData/dao/InfoCommentMapper.java
@@ -0,0 +1,13 @@
+package com.jiebao.platfrom.common.GuideData.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jiebao.platfrom.common.GuideData.domain.InfoComment;
+import com.jiebao.platfrom.common.GuideData.domain.Unituser;
+
+
+public interface InfoCommentMapper extends BaseMapper<InfoComment> {
+
+
+
+
+}
