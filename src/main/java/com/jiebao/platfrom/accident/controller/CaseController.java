74c5c0e153c67d1a4e7736334b1a7f751171d91a
Administrator <19918830011@163.com>
2020-11-23 11:39:12 +0800

    2020 11/23  同期比较小数点魔位

--- src/main/java/com/jiebao/platfrom/accident/controller/CaseController.java
+++ src/main/java/com/jiebao/platfrom/accident/controller/CaseController.java
@@ -1,15 +1,13 @@
 package com.jiebao.platfrom.accident.controller;
 
 
-import com.jiebao.platfrom.accident.daomain.Accident;
 import com.jiebao.platfrom.accident.daomain.Case;
 import com.jiebao.platfrom.accident.service.ICaseService;
 import com.jiebao.platfrom.common.annotation.Log;
 import com.jiebao.platfrom.common.domain.JiebaoResponse;
 import com.jiebao.platfrom.common.domain.QueryRequest;
+import com.jiebao.platfrom.system.service.DeptService;
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiImplicitParam;
-import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * <p>
@@ -33,12 +30,20 @@ import java.util.List;
 public class CaseController {
     @Autowired
     ICaseService caseService;
+    @Autowired
+    DeptService deptServices;
 
     @PostMapping("saveOrUpdate")
     @ApiOperation("添加修改 事故信息")
     @Log("添加修改 事故信息")
     public JiebaoResponse saveOrUpdate(Case ca) {
         JiebaoResponse jiebaoResponse = new JiebaoResponse();
+        if (ca.getPoliceId() != null) {
+            if (deptServices.getById(ca.getPoliceId()).getRank() == 4) {
+                return jiebaoResponse.failMessage("请选中对应的派出所");
+            }
+         //   accident.setPoliceFather(deptServices.getById(accident.getPoliceId()).getParentId());
+        }
         if (ca.getCaseId() != null) {
             Case aCase = caseService.getById(ca.getCaseId());
             if (aCase.getStatu() != null && aCase.getStatu() == 1)
@@ -67,8 +72,8 @@ public class CaseController {
     @GetMapping("listPage")
     @ApiOperation("条件查询分页  事故信息")
     @Log("条件查询分页  事故信息")
-    public JiebaoResponse list(QueryRequest queryRequest, String policeId, String cityLevelId,String quDeptId, String lineId, String nature, String startDate, String endDate) {
-        return caseService.list(queryRequest, policeId, cityLevelId,quDeptId, lineId, nature, startDate, endDate);
+    public JiebaoResponse list(QueryRequest queryRequest, String policeId, String cityLevelId, String quDeptId, String lineId, String nature, String startDate, String endDate) {
+        return caseService.list(queryRequest, policeId, cityLevelId, quDeptId, lineId, nature, startDate, endDate);
     }
 
     @GetMapping("map")
--- src/main/java/com/jiebao/platfrom/accident/dao/CaseMapper.java
+++ src/main/java/com/jiebao/platfrom/accident/dao/CaseMapper.java
@@ -1,11 +1,10 @@
 package com.jiebao.platfrom.accident.dao;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import com.jiebao.platfrom.accident.daomain.Accident;
 import com.jiebao.platfrom.accident.daomain.Case;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.*;
 
 import java.util.List;
@@ -25,7 +24,7 @@ public interface CaseMapper extends BaseMapper<Case> {
             @Result(property = "deptQx", column = "city_qx_id", one = @One(select = "com.jiebao.platfrom.system.dao.DeptMapper.selectById")),
             @Result(property = "deptXc", column = "city_xc", one = @One(select = "com.jiebao.platfrom.system.dao.DeptMapper.selectById")),
             @Result(property = "deptPolice", column = "police_id", one = @One(select = "com.jiebao.platfrom.system.dao.DeptMapper.selectById")),
-            @Result(property = "dictXl", column = "line_id", one = @One(select = "com.jiebao.platfrom.system.dao.DictMapper.selectById")),
+            @Result(property = "                                                                                                                                                                     cccc", column = "line_id", one = @One(select = "com.jiebao.platfrom.system.dao.DictMapper.selectById")),
     })
     IPage<Case> ListPage(Page<Case> page, @Param("ew") QueryWrapper<Case> queryWrapper);
 
